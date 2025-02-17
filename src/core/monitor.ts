6bd8c565907d4c7dc1e88ab9087a581b98826283
李荣熔 <13443892+a154488546@user.noreply.gitee.com>
2025-02-17 19:32:29 +0800

    feat: 添加网页停留时长函数

--- src/core/monitor.ts
+++ src/core/monitor.ts
@@ -1,15 +1,20 @@
 //导入类型定义
 import { MonitorOptions, MonitorEvent } from './types';
 import reportData from '../report/index'
-import PerformanceMonitor from '../plugins/performance/index';
-import { PVTracker } from '../plugins/performance/utils/pvuv';
-import { pushFlowData } from '../api/index';
+import PerformanceMonitor from '@/plugins/performance/index';
+//引入接口定义
+import { DurationData } from '@/api/interface';
+//引入pvuv和停留时长监控类
+import { PVTracker } from '@/plugins/performance/utils/pvuv';
+import { DurationTracker } from '@/plugins/performance/utils/duration';
+import { pushFlowData } from '@/api/index';
 
 export class Monitor {
   private options: MonitorOptions;
   private performanceMonitor: PerformanceMonitor; // 持有性能监控实例
   private queue: MonitorEvent[] = []; // 事件队列
   private pvTracker?: PVTracker; // 添加PV追踪实例
+  private durationTracker: DurationTracker; // 添加 DurationTracker 实例
 
   constructor(options: MonitorOptions) {
     this.options = {
@@ -18,14 +23,19 @@ export class Monitor {
     };
     this.performanceMonitor = new PerformanceMonitor(); // 创建性能监控实例
     this.init();
+
     // 初始化PV追踪
     this.pvTracker = new PVTracker({
       reportHandler: this.handlePVReport.bind(this)
-      
+    });
+
+    // 初始化 DurationTracker
+    this.durationTracker = new DurationTracker({
+      reportHandler: this.handleDurationReport.bind(this)
     });
   }
 
-  // 统一处理PV上报
+  // 处理PV上报
   private async handlePVReport(pagePath: string) {
     try {
       await pushFlowData(pagePath, 'pv');
@@ -36,12 +46,22 @@ export class Monitor {
     }
   }
 
+  // 处理页面停留时长上报
+  private async handleDurationReport(data: DurationData) {
+    try {
+      await pushFlowData(data.pagePath, 'duration', data.duration); // 假设 pushFlowData 支持上报停留时长
+      console.log('页面停留时长数据上报成功');
+    } catch (error) {
+      console.error('页面停留时长数据上报失败:', error);
+    }
+  }
+
   //初始化会发送数据到数据库
   private init(): void {
     console.log('初始化监听。。。');
-    // 延迟上报（确保性能数据已收集）
+    // 延迟上报（确保性能监控数据已收集）
     setTimeout(async () => {
-      // 合并性能数据和其他数据（如错误）
+      // 合并各性能监控数据
       const reportPvuv = {
         performance: this.performanceMonitor.getMetrics(),
         event: this.queue
@@ -63,7 +83,7 @@ export class Monitor {
       // });
     }, this.options.delay);
 
-    
+
     // 其他初始化逻辑（如错误监听）
   }
 
